30e53e7d7007c22ac7756ce4a02ce4d9d969ca2a
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2021-12-10 10:31:25 +0100

    Creat HTML´s  and JV´s files

---
+++ formulario.js
@@ -0,0 +1,19 @@
+'use strict';
+
+window.addEventListener("load", () =>{
+
+    var form = document.querySelector('[customName="form"]');
+
+    var button = document.querySelector('[customName="btn"]');
+
+    button.addEventListener("submit", () =>{
+
+    });
+
+    button.addEventListener("click", () =>{
+        
+    });
+
+
+
+});
