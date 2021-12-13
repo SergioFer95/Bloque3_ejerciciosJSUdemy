98e5b5c47b95975621c6517419240fca29519c9d
Sergio Fernández <sergio.fernndez@bootcamp-upgrade.com>
2021-12-13 09:16:46 +0100

    Add other exercise / Finish this exercise too

--- formulario.js
+++ formulario.js
@@ -15,6 +15,29 @@ window.addEventListener("load", () => {
         var answerSurname = document.querySelector("#surname").value;
         var answerAge = document.querySelector("#age").value;
 
+        // Validación del formulario:
+
+        if(answerName.trim() == null || answerName.trim().length == 0){
+            alert("El nombre no es váido");
+            return false;
+        }
+        
+        if(answerSurname.trim() == null || answerName.trim().length == 0){
+            alert("El Apellido no es váido");
+            return false;
+        } 
+
+        if(answerAge.trim() == null || answerName.trim().length == 0 || answerAge == isNaN){
+            alert("La edad no es váida");
+            return false;
+        }
+
+        // Para que esto fuese funcional, el evento debería de ser "submit" en vez de "click". El problema es que si usaba el evento "submit", me enviaba la información y no la recogía de la forma que el ejercicio me pedía, por lo que he preferido usar este otro evento "click" para realizar un "simulacro" de envio de formulario.
+
+
+        // Fin de la validación del formulario
+
+
         user = {
             name: answerName,
             surname: answerSurname,
@@ -29,9 +52,6 @@ window.addEventListener("load", () => {
         paragraph.append("Edad: " + answerAge);
 
         results.append(paragraph);
-
-
-
 
         return console.log(user);
     });
